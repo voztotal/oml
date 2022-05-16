423e801c4bc48467270dc405da9bee627ba29e26
fpignataro <fabian.pignataro@protonmail.com>
2022-05-16 11:41:45 -0300

    oml 2.0 podman intro

---
+++ install/docker/prodenv/deploy/roles/prerequisitos/templates/omnileads_envars.sh
@@ -0,0 +1,68 @@
+CALLREC_DEVICE={{ callrec_device }}
+AMI_USER={{ ami_user }}
+AMI_PASSWORD={{ ami_password }}
+ASTERISK_HOSTNAME={{ asterisk_host }}
+ASTERISK_LOCATION={{ asterisk_location }}
+CALIFICACION_REAGENDA={{ schedule }}
+DJANGO_SETTINGS_MODULE=ominicontacto.settings.production
+EPHEMERAL_USER_TTL={{ ECCTL }}
+{% if extern_ip == "auto" %}
+EXTERN_IP={{ public_ip }}
+{% else %}
+EXTERN_IP={{ extern_ip }}
+{% endif %}
+INSTALL_PREFIX={{ install_prefix }}
+MONITORFORMAT={{ MONITORFORMAT }}
+{% if mysql_host is defined %}
+MYSQL_HOST={{ mysql_host }}
+{% endif %}
+LOGIN_FAILURE_LIMIT={{ LOGIN_FAILURE_LIMIT }}
+OMNILEADS_HOSTNAME=localhost
+PGHOST={{ postgres_host }}
+PGDATABASE={{ postgres_database }}
+PGPORT={{ postgres_port }}
+PGUSER={{ postgres_user }}
+PGPASSWORD={{ postgres_password }}
+PYTHONPATH=$INSTALL_PREFIX
+REDIS_HOSTNAME={{ redis_host }}
+RTPENGINE_HOSTNAME={{ rtpengine_host }}
+SESSION_COOKIE_AGE={{ SCA }}
+KAMAILIO_HOSTNAME={{ kamailio_host }}
+{% if kamailio_host == omni_fqdn %}
+KAMAILIO_CERTS_LOCATION={{ certs_location }}
+SHM_SIZE={{ shm_size }}
+PKG_SIZE={{ pkg_size }}
+{% endif %}
+TZ={{ TZ }}
+WEBSOCKET_HOST={{ websocket_host }}
+WEBSOCKET_PORT={{ websocket_port }}
+{% if dialer_host is defined %}
+WOMBAT_HOSTNAME={{ dialer_host }}
+{% else %}
+WOMBAT_HOSTNAME=localhost
+{% endif %}
+WOMBAT_USER={{ dialer_user }}
+WOMBAT_PASSWORD={{ dialer_password }}
+#S3 Bucket CALLREC envars
+{% if s3_bucket_name is defined %}
+S3_STORAGE_ENABLED=true
+S3_BUCKET_NAME={{ s3_bucket_name }}
+AWS_ACCESS_KEY_ID={{ s3_access_key }}
+AWS_SECRET_ACCESS_KEY={{ s3_secret_key }}
+S3_REGION_NAME={{ s3_region }}
+{% if callrec_device != "s3-aws" %}
+S3_ENDPOINT={{ s3url }}
+AWS_DEFAULT_REGION=us-east-1
+{% endif %}
+{% endif %}
+# Google maps API
+{% if google_maps_api_key is defined %}
+GOOGLE_MAPS_API_KEY={{ google_maps_api_key }}
+GOOGLE_MAPS_CENTER='{{ google_maps_center }}'
+{% endif %}
+# OML version envars
+OML_BRANCH={{ oml_release }}
+OML_COMMIT={{ commit }}
+OML_BUILD_DATE="{{ build_date }}"
+
+export CALLREC_DEVICE AMI_USER AMI_PASSWORD ASTERISK_HOSTNAME ASTERISK_LOCATION CALIFICACION_REAGENDA DJANGO_SETTINGS_MODULE EPHEMERAL_USER_TTL {% if extern_ip != "none" and extern_ip != "auto" %} EXTERN_IP {% endif %} INSTALL_PREFIX KAMAILIO_HOSTNAME {% if kamailio_host == omni_fqdn %} KAMAILIO_CERTS_LOCATION SHM_SIZE PKG_SIZE {% endif %} LOGIN_FAILURE_LIMIT MONITORFORMAT OMNILEADS_HOSTNAME PGHOST PGDATABASE PGUSER PGPASSWORD PGPORT PYTHONPATH REDIS_HOSTNAME RTPENGINE_HOSTNAME SESSION_COOKIE_AGE TZ WOMBAT_HOSTNAME WOMBAT_USER WOMBAT_PASSWORD {% if mysql_host is defined %}MYSQL_HOST{% endif %} OML_BRANCH OML_COMMIT OML_BUILD_DATE WEBSOCKET_HOST WEBSOCKET_PORT {% if callrec_device != "local" %} S3_STORAGE_ENABLED S3_BUCKET_NAME AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY S3_REGION_NAME {% if callrec_device != "s3-aws" %} S3_ENDPOINT AWS_DEFAULT_REGION {% endif %} {% endif %} {% if google_maps_api_key is defined %} GOOGLE_MAPS_API_KEY GOOGLE_MAPS_CENTER {% endif %}
---
+++ install/docker/prodenv/deploy/user-data.sh
@@ -0,0 +1,367 @@
+#!/bin/bash
+
+########################## README ############ README ############# README #########################
+# El script first_boot_installer, tiene como finalidad desplegar el componente sobre una instancia
+# de linux exclusiva. Las variables que utiliza son "variables de entorno" de la instancia que está
+# por lanzar el script como acto seguido al primer boot del sistema operativo.
+# Dichas variables podrán ser provisionadas por un archivo .env (ej: Vagrant) o bien utilizando este
+# script como plantilla de terraform.
+#
+# En el caso de necesitar ejecutar este script manualmente sobre el user_data de una instancia cloud
+# o bien sobre una instancia onpremise a través de una conexión ssh, entonces se deberá copiar
+# esta plantilla hacia un archivo ignorado por git (first_boot_installer.sh) para luego sobre
+# dicha copia, descomentar las líneas que comienzan con la cadena "export", para posteriormente
+# introducir el valor deseado a cada variable.
+########################## README ############ README ############# README #########################
+
+# ******************** SET ENV VARS ******************** #
+
+# The infrastructure environment:
+# Values: onpremise | digitalocean | linode | vultr | aws
+#export oml_infras_stage=onpremise
+
+# The GitLab branch
+#export oml_app_release=release-1.21.0
+
+# OMniLeads tenant NAME
+#export oml_tenant_name=onpremise
+
+# Device for recordings
+# Values: local | s3 | s3-minio | s3-aws | nfs | disk
+#export oml_callrec_device=local
+
+# Parameters for S3 when s3 is selected as store for oml_callrec_device
+# s3_access_key | NULL
+#export s3_access_key=
+# s3_access_secret_key | NULL
+#export s3_secret_key=
+# se bucket name | NULL
+#export s3_bucket_name=
+# s3 endpoint url Only when use non AWS S3 object | NULL
+#export s3_enpoint_url=NULL
+# s3 bucket region | NULL
+#export s3_region=NULL
+
+# Parameters for NFS when nfs is selected as store for oml_callrec_device
+# NFS netaddr | NULL
+#export nfs_host=
+
+# ******* persistent data STORE block devices *******
+
+# Values: /dev/disk/by-label/optoml-${oml_tenant_name}
+#export optoml_device=NULL
+# Values: /dev/disk/by-label/pgsql-${oml_tenant_name}
+#export pgsql_device=NULL
+
+# Set your network interface
+#export oml_nic=eth0
+
+# ******* Variables for ACD Asterisk *******
+# AMI connection from OMLApp
+#export oml_ami_user=omnileadsami
+#export oml_ami_password=098098ZZZ
+# Values: NULL | IP address or FQDN
+#export oml_acd_host=NULL
+
+# ******* Variables for PGSQL *******
+# POSTGRESQL network address and port
+# Values: NULL | IP address or FQDN
+#export oml_pgsql_host=NULL
+#export oml_pgsql_port=5432
+# POSTGRESQL user, password and DB parameters
+#export oml_pgsql_db=omnileads
+#export oml_pgsql_user=omnileads
+#export oml_pgsql_password=098098ZZZ
+# If PGSQL runs on cloud cluster, set this parameter to true
+#export oml_pgsql_cloud=NULL
+
+# ******* Variables for Dialer *******
+#export api_dialer_user=demoadmin
+#export api_dialer_password=demo
+# Values: NULL | IP address or FQDN
+#export oml_dialer_host=NULL
+
+# ******* Variables for WebRTC bridge *******
+# Values: NULL | IP address or FQDN
+#export oml_rtpengine_host=NULL
+# Values: NULL | IP address or FQDN
+#export oml_kamailio_host=NULL
+
+# ******* Variables for Redis and Websocket *******
+# Values: NULL | IP address or FQDN
+#export oml_redis_host=NULL
+# Values: NULL | IP address or FQDN
+#export oml_websocket_host=NULL
+#export oml_websocket_port=NULL
+
+# *********************** NAT voip webrtc setting ***************************************************************************************
+# External IP. This parameter will set the public IP for SIP and RTP traffic, on environments where calls go through a firewall.        #
+# Values: auto | IP address | none                                                                                                      #
+# auto = The public IP will be obtained from http://ipinfo.io/ip. It depends on the WAN connection that OML is using to go to Internet. #
+# X.X.X.X = The public IP is set manually.                                                                                              #
+# none = If the agents are working on a LAN environment, and don't need a public IP.                                                    #
+# ***************************************************************************************************************************************
+#export oml_extern_ip=none
+
+# ******* Vaiables for OMLApp web *******
+#export oml_tz=America/Argentina/Cordoba
+# Session Cookie Age (SCA): It's the time in seconds that will last the https session when inactivity
+# is detected in the session (by default is 1 hour)
+#export oml_app_sca=3600
+# Ephemeral Credentials TTL (ECTTL): It's the time in seconds that will last the SIP credentials
+# used to authenticate a SIP user in the telephony system (by default 8 hours)
+#export oml_app_ecctl=28800
+# Login failure limit (LFM): It's the number of attempts a user has to enter an incorrect password in login
+# Decrease it if paranoic reasons
+#export oml_app_login_fail_limit=10
+
+# Values: true | false
+#export oml_app_init_env=true
+#export oml_app_reset_admin_pass=true
+#export oml_app_install_sngrep=true
+
+# In case of infrastructure as code deploy set the omlapp-backup-filename
+# Onpremise deploy all "NULL"
+# Values: backup file name (without .tar.gz) or NULL
+#export oml_backup_filename=NULL
+# Values: true | NULL
+#export oml_auto_restore=NULL
+
+# Above 200 users enable this
+# Values: true | NULL
+#export oml_high_load=NULL
+
+# Google maps API
+#export oml_google_maps_api_key=NULL
+#export oml_google_maps_center='{ "lat": -31.416668, "lng": -64.183334 }'
+
+
+oml_app_repo_url=https://gitlab.com/omnileads/ominicontacto.git
+SRC=/usr/src
+PATH_DEPLOY=install/onpremise/deploy/ansible
+CALLREC_DIR_DST=/opt/omnileads/asterisk/var/spool/asterisk/monitor
+
+# if callrec device like DISK BLOCK DEVICE
+if [[ ${oml_callrec_device} == "disk" ]];then
+  CALLREC_BLOCK_DEVICE=/dev/disk/by-label/callrec-${oml_tenant_name}
+fi
+
+echo "******************** OML RELEASE = ${oml_app_release} ********************"
+
+sleep 5
+
+echo "******************** block_device mount ********************"
+
+if [[ ${pgsql_device} != "NULL" ]];then
+  mkdir /var/lib/pgsql
+  echo "${pgsql_device} /var/lib/pgsql ext4 defaults,nofail,discard 0 0" >> /etc/fstab
+fi
+
+mount -a
+sleep 10
+mount
+
+echo "******************** IPV4 address config ********************"
+
+case ${oml_infras_stage} in
+  aws)
+    echo -n "AWS"
+    PRIVATE_IPV4=$(ip addr show ${oml_nic} | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+    PUBLIC_IPV4=$(curl ifconfig.co)
+    ;;
+  digitalocean)
+    echo -n "DigitalOcean"
+    PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+    PRIVATE_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address)
+    ;;
+  linode)
+    echo -n "Linode"
+    PRIVATE_IPV4=$(ip addr show ${oml_nic} |grep "inet 192.168" |awk '{print $2}' | cut -d/ -f1)
+    PUBLIC_IPV4=$(curl checkip.amazonaws.com)
+    ;;
+  onpremise)
+    echo -n "Onpremise CentOS7 Minimal"
+    PRIVATE_IPV4=$(ip addr show ${oml_nic} | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+    PUBLIC_IPV4=$(curl ifconfig.co)
+    ;;
+  vagrant)
+    echo -n "Vagrant CentOS7 Minimal CI/CD"
+    PRIVATE_IPV4=$STAGING_IP_CENTOS
+    PUBLIC_IPV4=$(curl ifconfig.co)
+    ;;
+  *)
+    echo -n "You must to set STAGE variable\n"
+    ;;
+esac
+
+echo "******************** STAGE fix /etc/hosts ********************"
+
+case ${oml_infras_stage} in
+  digitalocean)
+    echo -n "DigitalOcean"
+    sed -i 's/127.0.0.1 '$(hostname)'/#127.0.0.1 '$(hostname)'/' /etc/hosts
+    sed -i 's/::1 '$(hostname)'/#::1 '$(hostname)'/' /etc/hosts
+    ;;
+  vultr)
+    echo -n "Vultr"
+    TEMP_HOSTNAME=$(hostname)
+    sed -i 's/127.0.0.1 '$TEMP_HOSTNAME'/#127.0.0.1 '$TEMP_HOSTNAME'/' /etc/hosts
+    sed -i 's/::1       '$TEMP_HOSTNAME'/#::1 '$TEMP_HOSTNAME'/' /etc/hosts
+    ;;
+  *)
+    echo -n "Your stage is clean.\n"
+    ;;
+esac
+
+echo "******************** SELinux and firewalld disable ********************"
+
+# setenforce 0
+# sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/sysconfig/selinux
+# sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+# systemctl disable firewalld > /dev/null 2>&1
+# systemctl stop firewalld > /dev/null 2>&1
+
+echo "******************** Ansible installation ********************"
+
+sudo apt install -y software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt update
+sudo apt install -y ansible
+# sudo apt install python3-argcomplete
+# sudo activate-global-python-argcomplete3
+
+
+echo "******************** git clone omnileads repo ********************"
+
+cd $SRC
+git clone --recurse-submodules --branch ${oml_app_release} ${oml_app_repo_url}
+cd ominicontacto
+git submodule update --remote
+
+echo "******************** inventory setting ********************"
+
+sed -i "s/#localhost ansible/localhost ansible/g" $PATH_DEPLOY/inventory
+
+# PGSQL edit inventory params **************************************************
+
+if [[ "${oml_pgsql_cloud}"  == "true" ]];then
+  sed -i "s/postgres_cloud=false/postgres_cloud=true/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_pgsql_db}"  != "NULL" ]];then
+  sed -i "s/postgres_database=omnileads/postgres_database=${oml_pgsql_db}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_pgsql_user}"  != "NULL" ]];then
+  sed -i "s/#postgres_user=omnileads/postgres_user=${oml_pgsql_user}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_pgsql_password}"  != "NULL" ]];then
+  sed -i "s/#postgres_password=my_very_strong_pass/postgres_password=${oml_pgsql_password}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_pgsql_host}"  != "NULL" ]];then
+  sed -i "s/#postgres_host=/postgres_host=${oml_pgsql_host}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_pgsql_port}"  != "NULL" ]];then
+  sed -i "s/#postgres_port=/postgres_port=${oml_pgsql_port}/g" $PATH_DEPLOY/inventory
+fi
+
+# Asterisk ACD parameters *******
+
+if [[ "${oml_ami_user}"  != "NULL" ]];then
+  sed -i "s/#ami_user=omnileadsami/ami_user=${oml_ami_user}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_ami_password}"  != "NULL" ]];then
+  sed -i "s/#ami_password=5_MeO_DMT/ami_password=${oml_ami_password}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_acd_host}"  != "NULL" ]];then
+  sed -i "s/#asterisk_host=/asterisk_host=${oml_acd_host}/g" $PATH_DEPLOY/inventory
+fi
+
+# Wombat Dialer parameters *******
+
+if [[ "${api_dialer_user}"  != "NULL" ]];then
+  sed -i "s/#dialer_user=demoadmin/dialer_user=${api_dialer_user}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${api_dialer_password}"  != "NULL" ]];then
+  sed -i "s/#dialer_password=demo/dialer_password=${api_dialer_password}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_dialer_host}" != "NULL" ]];then
+  sed -i "s/#dialer_host=/dialer_host=${oml_dialer_host}/g" $PATH_DEPLOY/inventory
+fi
+
+# WebRTC kamailio & rtpengine params *******
+
+if [[ "${oml_kamailio_host}"  != "NULL" ]];then
+  sed -i "s/#kamailio_host=/kamailio_host=${oml_kamailio_host}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_rtpengine_host}" != "NULL" ]];then
+  sed -i "s/#rtpengine_host=/rtpengine_host=${oml_rtpengine_host}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_extern_ip}" != "NULL" ]];then
+  sed -i "s/#extern_ip=auto/extern_ip=${oml_extern_ip}/g" $PATH_DEPLOY/inventory
+fi
+
+# Redis, Nginx and Websockets params *******
+
+if [[ "${oml_redis_host}" != "NULL" ]];then
+  sed -i "s/#redis_host=/redis_host=${oml_redis_host}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "$NGINX_HOST" != "NULL" ]];then
+  sed -i "s/#nginx_host=/nginx_host=$NGINX_HOST/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_websocket_host}" != "NULL" ]];then
+  sed -i "s/#websocket_host=/websocket_host=${oml_websocket_host}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_websocket_port}" != "NULL" ]];then
+  sed -i "s/#websocket_port=/websocket_port=${oml_websocket_port}/g" $PATH_DEPLOY/inventory
+fi
+
+# Others App params *******
+
+sed -i "s%\#TZ=%TZ=${oml_tz}%g" $PATH_DEPLOY/inventory
+
+if [[ "$${oml_app_sca}" != "NULL" ]];then
+  sed -i "s/sca=3600/sca=${oml_app_sca}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_app_ecctl}" != "NULL" ]];then
+  sed -i "s/sca=28800/sca=${oml_app_ecctl}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_app_login_fail_limit}" != "NULL" ]];then
+  sed -i "s/LOGIN_FAILURE_LIMIT=10/LOGIN_FAILURE_LIMIT=${oml_app_login_fail_limit}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_app_reset_admin_pass}" == "true" ]];then
+  sed -i "s/reset_admin_password=false/reset_admin_password=true/g" $PATH_DEPLOY/inventory
+fi
+
+if [[ "${oml_callrec_device}" != "NULL" ]];then
+sed -i "s/callrec_device=local/callrec_device=${oml_callrec_device}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_backup_filename}" != "NULL" ]];then
+sed -i "s%\#backup_file_name=%backup_file_name=${oml_backup_filename}%g" $PATH_DEPLOY/inventory
+fi
+if [[ "${s3_access_key}" != "NULL" ]];then
+sed -i "s%\#s3_access_key=%s3_access_key=${s3_access_key}%g" $PATH_DEPLOY/inventory
+fi
+if [[ "${s3_secret_key}" != "NULL" ]];then
+sed -i "s%\#s3_secret_key=%s3_secret_key=${s3_secret_key}%g" $PATH_DEPLOY/inventory
+fi
+if [[ "${s3_bucket_name}" != "NULL" ]];then
+sed -i "s%\#s3_bucket_name=%s3_bucket_name=${s3_bucket_name}%g" $PATH_DEPLOY/inventory
+fi
+if [[ "${s3_endpoint_url}" != "NULL" ]];then
+sed -i "s%\#s3url=%s3url=${s3_endpoint_url}%g" $PATH_DEPLOY/inventory
+fi
+if [[ "${s3_region}" != "NULL" ]];then
+sed -i "s%\#s3_region=%s3_region=${s3_region}%g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_auto_restore}" != "NULL" ]];then
+sed -i "s/auto_restore=false/auto_restore=${oml_auto_restore}/g" $PATH_DEPLOY/inventory
+fi
+if [[ "${oml_high_load}" != "NULL" ]];then
+sed -i "s/high_load=false/high_load=${oml_high_load}/g" $PATH_DEPLOY/inventory
+fi
+
+if [[ "${oml_google_maps_api_key}" != "NULL" ]];then
+sed -i "s%\#google_maps_api_key=%google_maps_api_key=${oml_google_maps_api_key}%g" $PATH_DEPLOY/inventory
+sed -i "s%\#google_maps_center=%google_maps_center='${oml_google_maps_center}'%g" $PATH_DEPLOY/inventory
+fi
+
+ansible-playbook omnileads.yml -i inventory 
