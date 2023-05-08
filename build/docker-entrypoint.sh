179de725378bbac86bf8f008539cedaaa2891009
fpignataro <fabian.pignataro84@gmail.com>
2023-05-08 12:25:35 -0300

    uwsgi daphne ports

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -48,7 +48,7 @@ EOF
 Init_ASGI() {
     cat /dev/null > /var/spool/cron/crontabs/root
     echo "Iniciando Daphne ASGI"
-    exec /usr/local/bin/daphne -p 8098 -b ${DJANGO_HOSTNAME} --proxy-headers --verbosity=3 ominicontacto.asgi:application
+    exec /usr/local/bin/daphne -p ${DAPHNE_PORT} -b ${DJANGO_HOSTNAME} --proxy-headers --verbosity=3 ominicontacto.asgi:application
 }
 
 if [ "$1" = "" ]; then
